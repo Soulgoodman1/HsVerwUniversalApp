b2a8cd9e6715c88a4ec76ffc97ba13b80e5273a2
Ralf Abels <gallatin17@ralfabels.de>
2015-12-31 11:44:17 +0100

    Erstcommit Start Quellodeverwaltung

---
+++ HsVerwUniversalApp/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿' Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+''' <summary>
+''' Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Public Sub New()
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+
+
+        Dim vlo_client As New HsVerwSvc.Service1Client
+        Dim _hhkatresult As Task(Of ObservableCollection(Of HsVerwSvc.Haushaltskategorie)) = vlo_client.GetHaushaltskategorienAsync
+
+        ListviewHsVw.ItemsSource = _hhkatresult.Result
+
+    End Sub
+
+    Private Sub btn_weiterzutest_Click(sender As Object, e As RoutedEventArgs) Handles btn_weiterzutest.Click
+
+        ' Zur neuen Seite navigieren
+        Frame.Navigate(GetType(TestHub))
+
+    End Sub
+End Class
