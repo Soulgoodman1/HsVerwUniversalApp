a9290a8307150e8ebdd078b5f88d18295a7b3940
gallatin17 <gasllatin17@ralfabels.de>
2016-01-06 19:53:35 +0100

    Auflistungen zu HubSections erweitert (Proof of Concept), Buttons für Auflistungen separiert und Routine für Visibility added

---
+++ HsVerwUniversalApp/ListExpense.xaml.vb
@@ -0,0 +1,24 @@
+﻿' Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+''' <summary>
+''' Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+''' </summary>
+Public NotInheritable Class ListExpense
+    Inherits Page
+
+    Public Sub New()
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+
+
+        Dim vlo_client As New HsVerwSvc.Service1Client
+        Dim _hhkatresult As Task(Of ObservableCollection(Of HsVerwSvc.Ausgabe)) = vlo_client.GetAusgabenAsync
+        Dim vlo_ausgabe As New HsVerwSvc.Ausgabe
+
+        ListviewExpense.ItemsSource = _hhkatresult.Result
+
+    End Sub
+
+End Class
