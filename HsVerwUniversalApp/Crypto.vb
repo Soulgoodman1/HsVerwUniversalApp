eb82ea207754a3be51a4bbc6f6178c5210262ae6
gallatin17 <gasllatin17@ralfabels.de>
2016-02-04 19:24:44 +0100

    Grundlagen Nutzerkonten (Passwort, Hashing, Salt usw)

--- HsVerwUniversalApp/Crypto.vb
+++ HsVerwUniversalApp/Crypto.vb
@@ -1,44 +1,56 @@
-﻿
+﻿Imports Windows.Security.Cryptography
+Imports Windows.Security.Cryptography.Core
+Imports Windows.Storage.Streams
+Imports System.Text
+
+
 Public Class Crypto
 
+    Public Function CreateHash(ByVal password As String) As String
 
-    'Namespace PasswordHash
-    '{
-    '    /// <summary>
-    '    /// Salted password hashing with PBKDF2-SHA1.
-    '    /// Author: havoc AT defuse.ca
-    '    /// www: http://crackstation.net/hashing-security.htm
-    '    /// Compatibility: .NET 3.0 And later.
-    '    /// </summary>
-    '    Public Class PasswordHash
-    '    {
-    '        // The following constants may be changed without breaking existing hashes.
-    '        Public Const int SALT_BYTE_SIZE = 24;
-    '        Public Const int HASH_BYTE_SIZE = 24;
-    '        Public Const int PBKDF2_ITERATIONS = 1000;
+        'Append Salt
+        password = password & GenerateRandomData()
 
-    '        Public Const int ITERATION_INDEX = 0;
-    '        Public Const int SALT_INDEX = 1;
-    '        Public Const int PBKDF2_INDEX = 2;
+        'Hash Password und Salt
+        Dim objMd5Hasher As CryptographicHash = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5).CreateHash()
 
-    '        /// <summary>
-    '        /// Creates a salted PBKDF2 hash of the password.
-    '        /// </summary>
-    '        /// <param name="password">The password to hash.</param>
-    '        /// <returns>The hash of the password.</returns>
-    '        Public Static String CreateHash(String password)
-    '        {
-    '            // Generate a random salt
-    '            RNGCryptoServiceProvider csprng = New RNGCryptoServiceProvider();
-    '            Byte[] salt = New Byte[SALT_BYTE_SIZE];
-    '            csprng.GetBytes(salt);
+        Dim inputBuffer As IBuffer = CryptographicBuffer.ConvertStringToBinary(password, BinaryStringEncoding.Utf16LE)
 
-    '            // Hash the password And encode the parameters
-    '            Byte[] hash = PBKDF2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
-    '            Return PBKDF2_ITERATIONS + ":" +
-    '                Convert.ToBase64String(salt) + ":" +
-    '                Convert.ToBase64String(hash);
-    '        }
+        objMd5Hasher.Append(inputBuffer)
+
+        Dim buffHash As IBuffer = objMd5Hasher.GetValueAndReset()
+
+        Dim sb As StringBuilder = New StringBuilder()
+
+        Dim data As Byte() = Nothing
+
+        CryptographicBuffer.CopyToByteArray(buffHash, data)
+
+        For i As Integer = 0 To i < data.Length Step 1
+            sb.Append(data(i).ToString("x2"))
+        Next i
+
+        'Return salted Hash
+        Return sb.ToString()
+
+    End Function
+
+    'Generate a random salt
+    Public Function GenerateRandomData() As String
+
+        'Define the length, in bytes, of the buffer.
+        Dim length As Integer = 32
+
+        'Generate random data And copy it to a buffer
+        Dim Buffer As IBuffer = CryptographicBuffer.GenerateRandom(length)
+
+        'Encode the buffer to a hexadecimal string (for display)
+        Dim randomHex As String = CryptographicBuffer.EncodeToHexString(Buffer)
+
+        Return randomHex
+    End Function
+
+
 
     '        /// <summary>
     '        /// Validates a password given a hash of the correct one.
@@ -91,4 +103,9 @@ Public Class Crypto
     '        }
     '    }
     '}
+
+
+
+
+
 End Class
--- HsVerwUniversalApp/MainPage.xaml.vb
+++ HsVerwUniversalApp/MainPage.xaml.vb
@@ -73,6 +73,9 @@ Public NotInheritable Class MainPage
         End If
 
 
+        splashProgressRing.IsActive = False
+        splashProgressRing.Visibility = Visibility.Collapsed
+
         'To Store a Password
         '1.Generate a long random salt using a CSPRNG.
         '2.Prepend the salt to the password And hash it with a standard cryptographic hash function such as SHA256.
@@ -82,102 +85,6 @@ Public NotInheritable Class MainPage
         '1.Retrieve the user's salt and hash from the database.
         '2.Prepend the salt to the given password And hash it using the same hash function.
         '3.Compare the hash of the given password with the hash from the database. If they match, the password Is correct. Otherwise, the password Is incorrect.
-
-
-        '        Using System;
-        'Using System.Text;
-        'Using System.Security.Cryptography;
-
-        'Namespace PasswordHash
-        '{
-        '    /// <summary>
-        '    /// Salted password hashing with PBKDF2-SHA1.
-        '    /// Author: havoc AT defuse.ca
-        '    /// www: http://crackstation.net/hashing-security.htm
-        '    /// Compatibility: .NET 3.0 And later.
-        '    /// </summary>
-        '    Public Class PasswordHash
-        '    {
-        '        // The following constants may be changed without breaking existing hashes.
-        '        Public Const int SALT_BYTE_SIZE = 24;
-        '        Public Const int HASH_BYTE_SIZE = 24;
-        '        Public Const int PBKDF2_ITERATIONS = 1000;
-
-        '        Public Const int ITERATION_INDEX = 0;
-        '        Public Const int SALT_INDEX = 1;
-        '        Public Const int PBKDF2_INDEX = 2;
-
-        '        /// <summary>
-        '        /// Creates a salted PBKDF2 hash of the password.
-        '        /// </summary>
-        '        /// <param name="password">The password to hash.</param>
-        '        /// <returns>The hash of the password.</returns>
-        '        Public Static String CreateHash(String password)
-        '        {
-        '            // Generate a random salt
-        '            RNGCryptoServiceProvider csprng = New RNGCryptoServiceProvider();
-        '            Byte[] salt = New Byte[SALT_BYTE_SIZE];
-        '            csprng.GetBytes(salt);
-
-        '            // Hash the password And encode the parameters
-        '            Byte[] hash = PBKDF2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
-        '            Return PBKDF2_ITERATIONS + ":" +
-        '                Convert.ToBase64String(salt) + ":" +
-        '                Convert.ToBase64String(hash);
-        '        }
-
-        '        /// <summary>
-        '        /// Validates a password given a hash of the correct one.
-        '        /// </summary>
-        '        /// <param name="password">The password to check.</param>
-        '        /// <param name="correctHash">A hash of the correct password.</param>
-        '        /// <returns>True if the password Is correct. False otherwise.</returns>
-        '        Public Static bool ValidatePassword(String password, String correctHash)
-        '        {
-        '            // Extract the parameters from the hash
-        '            Char[] delimiter = { ':' };
-        '            String[] split = correctHash.Split(delimiter);
-        '            int iterations = Int32.Parse(split[ITERATION_INDEX]);
-        '            Byte[] salt = Convert.FromBase64String(split[SALT_INDEX]);
-        '            Byte[] hash = Convert.FromBase64String(split[PBKDF2_INDEX]);
-
-        '            Byte[] testHash = PBKDF2(password, salt, iterations, hash.Length);
-        '            Return SlowEquals(hash, testHash);
-        '        }
-
-        '        /// <summary>
-        '        /// Compares two byte arrays in length-constant time. This comparison
-        '        /// method Is used so that password hashes cannot be extracted from
-        '        /// on-line systems using a timing attack And then attacked off-line.
-        '        /// </summary>
-        '        /// <param name="a">The first byte array.</param>
-        '        /// <param name="b">The second byte array.</param>
-        '        /// <returns>True if both byte arrays are equal. False otherwise.</returns>
-        '        Private Static bool SlowEquals(Byte[] a, Byte[] b)
-        '        {
-        '            uint diff = (uint)a.Length ^ (uint)b.Length;
-        '            For (int i = 0; i < a.Length && i < b.Length; i++)
-        '                diff |= (uint)(a[i] ^ b[i]);
-        '            Return diff == 0;
-        '        }
-
-        '        /// <summary>
-        '        /// Computes the PBKDF2-SHA1 hash of a password.
-        '        /// </summary>
-        '        /// <param name="password">The password to hash.</param>
-        '        /// <param name="salt">The salt.</param>
-        '        /// <param name="iterations">The PBKDF2 iteration count.</param>
-        '        /// <param name="outputBytes">The length of the hash to generate, in bytes.</param>
-        '        /// <returns>A hash of the password.</returns>
-        '        Private Static Byte[] PBKDF2(String password, Byte[] salt, int iterations, int outputBytes)
-        '        {
-        '            Rfc2898DeriveBytes pbkdf2 = New Rfc2898DeriveBytes(password, salt);
-        '            pbkdf2.IterationCount = iterations;
-        '            Return pbkdf2.GetBytes(outputBytes);
-        '        }
-        '    }
-        '}
-
 
     End Sub
     Private Async Function TestServer() As Task(Of Boolean)
