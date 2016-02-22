7a87dc91af2277aad085a203e5d71ac38b98b0dd
gallatin17 <gasllatin17@ralfabels.de>
2016-02-22 17:31:44 +0100

    Version 1.0a

--- HsVerwUniversalApp/Crypto.vb
+++ HsVerwUniversalApp/Crypto.vb
@@ -55,62 +55,36 @@ Public Class Crypto
         Return randomHex
     End Function
 
+    Public Function TestPassword(ByVal password As String, ByVal hash As String, ByVal salt As String) As Boolean
+        Dim vlo_user As New HsVerwSvc.User
 
+        password = password & salt
 
-    '        /// <summary>
-    '        /// Validates a password given a hash of the correct one.
-    '        /// </summary>
-    '        /// <param name="password">The password to check.</param>
-    '        /// <param name="correctHash">A hash of the correct password.</param>
-    '        /// <returns>True if the password Is correct. False otherwise.</returns>
-    '        Public Static bool ValidatePassword(String password, String correctHash)
-    '        {
-    '            // Extract the parameters from the hash
-    '            Char[] delimiter = { ':' };
-    '            String[] split = correctHash.Split(delimiter);
-    '            int iterations = Int32.Parse(split[ITERATION_INDEX]);
-    '            Byte[] salt = Convert.FromBase64String(split[SALT_INDEX]);
-    '            Byte[] hash = Convert.FromBase64String(split[PBKDF2_INDEX]);
+        'Hash Password und Salt
+        Dim objMd5Hasher As CryptographicHash = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5).CreateHash()
 
-    '            Byte[] testHash = PBKDF2(password, salt, iterations, hash.Length);
-    '            Return SlowEquals(hash, testHash);
-    '        }
+        Dim inputBuffer As IBuffer = CryptographicBuffer.ConvertStringToBinary(password, BinaryStringEncoding.Utf16LE)
 
-    '        /// <summary>
-    '        /// Compares two byte arrays in length-constant time. This comparison
-    '        /// method Is used so that password hashes cannot be extracted from
-    '        /// on-line systems using a timing attack And then attacked off-line.
-    '        /// </summary>
-    '        /// <param name="a">The first byte array.</param>
-    '        /// <param name="b">The second byte array.</param>
-    '        /// <returns>True if both byte arrays are equal. False otherwise.</returns>
-    '        Private Static bool SlowEquals(Byte[] a, Byte[] b)
-    '        {
-    '            uint diff = (uint)a.Length ^ (uint)b.Length;
-    '            For (int i = 0; i < a.Length && i < b.Length; i++)
-    '                diff |= (uint)(a[i] ^ b[i]);
-    '            Return diff == 0;
-    '        }
+        objMd5Hasher.Append(inputBuffer)
 
-    '        /// <summary>
-    '        /// Computes the PBKDF2-SHA1 hash of a password.
-    '        /// </summary>
-    '        /// <param name="password">The password to hash.</param>
-    '        /// <param name="salt">The salt.</param>
-    '        /// <param name="iterations">The PBKDF2 iteration count.</param>
-    '        /// <param name="outputBytes">The length of the hash to generate, in bytes.</param>
-    '        /// <returns>A hash of the password.</returns>
-    '        Private Static Byte[] PBKDF2(String password, Byte[] salt, int iterations, int outputBytes)
-    '        {
-    '            Rfc2898DeriveBytes pbkdf2 = New Rfc2898DeriveBytes(password, salt);
-    '            pbkdf2.IterationCount = iterations;
-    '            Return pbkdf2.GetBytes(outputBytes);
-    '        }
-    '    }
-    '}
+        Dim buffHash As IBuffer = objMd5Hasher.GetValueAndReset()
 
+        Dim sb As StringBuilder = New StringBuilder()
 
+        Dim data As Byte() = Nothing
 
+        CryptographicBuffer.CopyToByteArray(buffHash, data)
 
+        For i As Integer = 0 To (data.Length - 1) Step 1
+            sb.Append(data(i).ToString("x2"))
+        Next i
+
+        If hash = sb.ToString() Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
 
 End Class
--- HsVerwUniversalApp/MainPage.xaml.vb
+++ HsVerwUniversalApp/MainPage.xaml.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Net.NetworkInformation
 Imports Windows.Networking.Sockets
+Imports System.Text.RegularExpressions
 Imports Windows.UI
 
 ' Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
@@ -73,19 +74,8 @@ Public NotInheritable Class MainPage
             ckb_server.IsChecked = False
         End If
 
-
         splashProgressRing.IsActive = False
         splashProgressRing.Visibility = Visibility.Collapsed
-
-        'To Store a Password
-        '1.Generate a long random salt using a CSPRNG.
-        '2.Prepend the salt to the password And hash it with a standard cryptographic hash function such as SHA256.
-        '3.Save both the salt And the hash in the user's database record.
-
-        'To Validate a Password
-        '1.Retrieve the user's salt and hash from the database.
-        '2.Prepend the salt to the given password And hash it using the same hash function.
-        '3.Compare the hash of the given password with the hash from the database. If they match, the password Is correct. Otherwise, the password Is incorrect.
 
     End Sub
     Private Async Function TestServer() As Task(Of Boolean)
@@ -118,43 +108,121 @@ Public NotInheritable Class MainPage
     End Sub
 
     Private Sub btn_login_Click(sender As Object, e As RoutedEventArgs) Handles btn_login.Click
+        Dim vlo_crypto As New Crypto
+        Dim vlo_client As New HsVerwSvc.Service1Client
+        Dim vlo_user As New HsVerwSvc.User
 
-        Frame.Navigate(GetType(MainHub))
+        Dim _hhsetresultuser As Task(Of HsVerwSvc.User) = vlo_client.GetUserAsync(username.Text)
+        vlo_user = CType(_hhsetresultuser.Result, HsVerwSvc.User)
 
-    End Sub
+        If vlo_user.IDUser <> 0 Then
+            If vlo_crypto.TestPassword(pwdbox.Password, vlo_user.hash, vlo_user.salt) Then
+                If vlo_user.isactive Then
+                    Frame.Navigate(GetType(MainHub))
+                Else
+                    'Nutzer nach Registrierung noch nicht aktiviert
+                    CType(usernameflyout.Content, TextBox).Text = "Nutzer noch nicht aktiviert"
+                    usernameflyout.ShowAt(username)
+                End If
+            Else
+                'Falsches Kennwort
+                CType(passwordflyout.Content, TextBox).Text = "Falsches Kennwort"
+                passwordflyout.ShowAt(pwdbox)
+            End If
+        Else
+            'Nutzer unbekannt
+            CType(usernameflyout.Content, TextBox).Text = "Nutzer unbekannt"
+            usernameflyout.ShowAt(username)
+        End If
 
-    Private Sub pwdbox_Tapped(sender As Object, e As TappedRoutedEventArgs) Handles pwdbox.Tapped
-        pwdbox.PasswordRevealMode = PasswordRevealMode.Hidden
-        pwdbox.Password = ""
-    End Sub
 
-    Private Sub pwdbox_PointerEntered(sender As Object, e As PointerRoutedEventArgs) Handles pwdbox.PointerEntered
-        pwdbox.PasswordRevealMode = PasswordRevealMode.Hidden
-        pwdbox.Password = ""
     End Sub
 
     Private Sub btn_register_Click(sender As Object, e As RoutedEventArgs) Handles btn_register.Click
         Dim vlo_crypto As New Crypto
         Dim vlo_user As New HsVerwSvc.User
         Dim vlo_client As New HsVerwSvc.Service1Client
-        'Prüfung Kennwort Komplexität
-        'Prüfung, ob Username eMail Adresse entspricht xxxx@xx.xx
+        'Pürfen, ob User bereits vorhanden
+        Dim _hhsetresultuser As Task(Of HsVerwSvc.User) = vlo_client.GetUserAsync(username.Text)
 
-        'Create Hash
-        vlo_user = vlo_crypto.CreateHash(pwdbox.Password)
-        vlo_user.isactive = False
-        vlo_user.username = username.Text
+        If CType(_hhsetresultuser.Result, HsVerwSvc.User).IDUser = 0 Then 'ID = 0 heisst kein Nutzer mit diesem NAmen gefunden
 
-        'nach CreateHash Salt, Hash, Username, isactive = false wegschreiben über Backend
-        'incl. Benachrichtigung 
+            'Prüfung Kennwort Komplexität
+            If ValidatePassword(pwdbox.Password) Then
+                'Prüfung, ob Username eMail Adresse entspricht xxxx@xx.xx
+                If IsValidEmail(username.Text) Then
+                    'Create Hash
+                    vlo_user = vlo_crypto.CreateHash(pwdbox.Password)
+                    vlo_user.isactive = False
+                    vlo_user.username = username.Text
 
-        Dim _hhsetresult As Task(Of Boolean) = vlo_client.SetUserNewAsync(vlo_user)
+                    'nach CreateHash Salt, Hash, Username, isactive = false wegschreiben über Backend
+                    'incl. Benachrichtigung 
 
-        If Not _hhsetresult.Result Then
-            Frame.GoBack()
-        End If
+                    Dim _hhsetresult As Task(Of Boolean) = vlo_client.SetUserNewAsync(vlo_user)
+
+                    If Not _hhsetresult.Result Then
+                        Frame.GoBack()
+                    End If
+                Else
+                    'Meldung keine gültige eMail Adresse
+                    CType(usernameflyout.Content, TextBox).Text = "eMail-Adresse prüfen"
+                    usernameflyout.ShowAt(username)
+                End If 'Username gültig
+            Else
+                'Meldung Kennwort nicht komplex genug(mind. 8, 1*Groß, 1*Klein, 1*Nummer)
+                CType(passwordflyout.Content, TextBox).Text = "Länge 8, mind. je 1 Groß/ Kleinbuchstabe, Nummer"
+                passwordflyout.ShowAt(pwdbox)
+            End If 'Password Complexity 
+        Else
+            'Meldung Nutzer bereits vorhanden
+            CType(usernameflyout.Content, TextBox).Text = "User bereits vorhanden"
+            usernameflyout.ShowAt(username)
+        End If 'Username vorhanden
 
         vlo_client = Nothing
 
     End Sub
+
+    Private Function IsValidEmail(strIn As String) As Boolean
+        ' Return true if strIn is in valid e-mail format.
+        Return Regex.IsMatch(strIn,
+              "^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
+              "(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$")
+    End Function
+
+    ''' <summary>Determines if a password is sufficiently complex.</summary>
+    ''' <param name="pwd">Password to validate</param>
+    ''' <param name="minLength">Minimum number of password characters.</param>
+    ''' <param name="numUpper">Minimum number of uppercase characters.</param>
+    ''' <param name="numLower">Minimum number of lowercase characters.</param>
+    ''' <param name="numNumbers">Minimum number of numeric characters.</param>
+    ''' <param name="numSpecial">Minimum number of special characters.</param>
+    ''' <returns>True if the password is sufficiently complex.</returns>
+    Private Function ValidatePassword(ByVal pwd As String,
+        Optional ByVal minLength As Integer = 8,
+        Optional ByVal numUpper As Integer = 1,
+        Optional ByVal numLower As Integer = 1,
+        Optional ByVal numNumbers As Integer = 2,
+        Optional ByVal numSpecial As Integer = 0) As Boolean
+
+        ' Replace [A-Z] with \p{Lu}, to allow for Unicode uppercase letters.
+        Dim upper As New System.Text.RegularExpressions.Regex("[A-Z]")
+        Dim lower As New System.Text.RegularExpressions.Regex("[a-z]")
+        Dim number As New System.Text.RegularExpressions.Regex("[0-9]")
+        ' Special is "none of the above".
+        Dim special As New System.Text.RegularExpressions.Regex("[^a-zA-Z0-9]")
+
+        ' Check the length.
+        If pwd.Length < minLength Then Return False
+        ' Check for minimum number of occurrences.
+        If upper.Matches(pwd).Count < numUpper Then Return False
+        If lower.Matches(pwd).Count < numLower Then Return False
+        If number.Matches(pwd).Count < numNumbers Then Return False
+        If special.Matches(pwd).Count < numSpecial Then Return False
+
+        ' Passed all checks.
+        Return True
+    End Function
+
 End Class
